7aae3e180c6d258c541343492dd947f9b6cb6abd
Yuichi Sato <satoyuichi80@gmail.com>
2022-12-31 15:41:04 +0900

    Section 3.3

---
+++ functions/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let x = five();
+
+    println!("The value of x is: {}", x);
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {}", x);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {}{}", value, unit_label);
+}
+
+fn five() -> i32 {
+    5
+}
